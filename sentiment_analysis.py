1640322332ece496355d810c8e2320234c28bc05
vatsalyabaluni <balunivatsalya2000@gmail.com>
2022-06-30 15:05:27 +0530

    make it better

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -31,11 +31,11 @@ def cleanText(text):
     text = re.sub(r'https?:\/\/\S+','',text) # Remove Hyperlinks
     return text
 
-# Function To get Subjectivity of the tweet
+
 def getSubjectivity(text):
     return round(TextBlob(text).sentiment.subjectivity,3)
 
-# Function to get Polarity of the tweet
+
 def getPolarity(text):
     return round(TextBlob(text).sentiment.polarity,3)
 
@@ -81,7 +81,6 @@ def plot(df,name):
     plt.xticks(rotation=0)
     plt.savefig('static/my_plot.png')
     plt.switch_backend('agg')
-    # plt.show()
 
 @app.route('/')
 def home():
@@ -95,16 +94,14 @@ def predict_user():
         count = request.form['count']
         fetched_tweets = get_user_tweets(api, user, count)
         plot(fetched_tweets,user)
+
         positive_tweets = fetched_tweets[fetched_tweets.Sentiment == 'Positive']
         negative_tweets = fetched_tweets[fetched_tweets.Sentiment == 'Negative']
         x = round((positive_tweets.shape[0] / fetched_tweets.shape[0]) * 100, 1)
         y = round((negative_tweets.shape[0] / fetched_tweets.shape[0]) * 100, 1)
         z = round(100 - (x + y), 1)
-        fetched_tweets = fetched_tweets.to_dict('records')
 
-        # print("Positive Sentiment Percentage :- " + str(x))
-        # print("Negative Sentiment Percentage :- " + str(y))
-        # print("Neutral Sentiment Percentage :- " + str(round(100 - (x + y), 1)))
+        fetched_tweets = fetched_tweets.to_dict('records')
 
         return render_template('result_user.html', result=fetched_tweets, pos=x,neg=y,neu=z)
 
@@ -125,11 +122,6 @@ def predict_tag():
 
         fetched_tweets = fetched_tweets.to_dict('records')
 
-
-        # print("Positive Sentiment Percentage :- " + str(x))
-        # print("Negative Sentiment Percentage :- " + str(y))
-        # print("Neutral Sentiment Percentage :- " + str(round(100 - (x + y), 1)))
-
         return render_template('result_user.html', result=fetched_tweets, pos=x, neg=y, neu=z)
 
 @app.route("/predict_sentence",methods=['POST','GET'])
@@ -143,19 +135,6 @@ def predict_text():
         return render_template('result_sentence.html',sentence=sentence,Subjectivity=Subjectivity,Polarity=Polarity,Sentiment=Sentiment)
 
 if __name__ == '__main__':
-
-    # API_Key = "YOfX4r7g3YCy5D2vE3BUG115I"
-    # API_Key_Secret = "GbTTmWA5JlEyPMJF9WCTMcyPHeNcXjHBREyjSYiX4iIyHwaLlZ"
-    # Access_Token = "1502904942344179718-or4wUdWtudoY9HX3hjLjJSmQpvCyBx"
-    # Access_Token_Secret = "AUFrsoATq5EBBNbpB4o3YdaIxppP1ghq3mfqtMZjGp9PC"
-    #
-    # try:
-    #     authenticate = tweepy.OAuthHandler(API_Key, API_Key_Secret)
-    #     authenticate.set_access_token(Access_Token, Access_Token_Secret)
-    #     api = tweepy.API(authenticate, wait_on_rate_limit=True)
-    #     api2 = api
-    # except:
-    #     print("Error: Authentication Failed")
 
     app.debug = True
     app.run()
