354aadf769bd2652c9f1056fcc54bc38822027bd
vatsalyabaluni <balunivatsalya2000@gmail.com>
2022-06-30 14:27:56 +0530

    make it better

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 from flask import Flask, render_template, redirect, url_for, request
 
-# api2 = None
+
 API_Key = "YOfX4r7g3YCy5D2vE3BUG115I"
 API_Key_Secret = "GbTTmWA5JlEyPMJF9WCTMcyPHeNcXjHBREyjSYiX4iIyHwaLlZ"
 Access_Token = "1502904942344179718-or4wUdWtudoY9HX3hjLjJSmQpvCyBx"
@@ -19,7 +19,7 @@ Access_Token_Secret = "AUFrsoATq5EBBNbpB4o3YdaIxppP1ghq3mfqtMZjGp9PC"
 authenticate = tweepy.OAuthHandler(API_Key, API_Key_Secret)
 authenticate.set_access_token(Access_Token, Access_Token_Secret)
 api = tweepy.API(authenticate, wait_on_rate_limit=True)
-# api2 = api
+
 
 app = Flask(__name__ , template_folder='templates')
 app.static_folder = 'static'
@@ -95,9 +95,18 @@ def predict_user():
         count = request.form['count']
         fetched_tweets = get_user_tweets(api, user, count)
         plot(fetched_tweets,user)
+        positive_tweets = fetched_tweets[fetched_tweets.Sentiment == 'Positive']
+        negative_tweets = fetched_tweets[fetched_tweets.Sentiment == 'Negative']
+        x = round((positive_tweets.shape[0] / fetched_tweets.shape[0]) * 100, 1)
+        y = round((negative_tweets.shape[0] / fetched_tweets.shape[0]) * 100, 1)
+        z = round(100 - (x + y), 1)
         fetched_tweets = fetched_tweets.to_dict('records')
 
-        return render_template('result_user.html', result=fetched_tweets)
+        # print("Positive Sentiment Percentage :- " + str(x))
+        # print("Negative Sentiment Percentage :- " + str(y))
+        # print("Neutral Sentiment Percentage :- " + str(round(100 - (x + y), 1)))
+
+        return render_template('result_user.html', result=fetched_tweets, pos=x,neg=y,neu=z)
 
 
 @app.route("/predict_tag", methods=['POST','GET'])
@@ -108,8 +117,20 @@ def predict_tag():
         fetched_tweets = get_hashtag_tweets(api,hash,count)
         plot(fetched_tweets,hash)
 
+        positive_tweets = fetched_tweets[fetched_tweets.Sentiment == 'Positive']
+        negative_tweets = fetched_tweets[fetched_tweets.Sentiment == 'Negative']
+        x = round((positive_tweets.shape[0] / fetched_tweets.shape[0]) * 100, 1)
+        y = round((negative_tweets.shape[0] / fetched_tweets.shape[0]) * 100, 1)
+        z = round(100 - (x + y), 1)
+
         fetched_tweets = fetched_tweets.to_dict('records')
-        return render_template('result_user.html', result=fetched_tweets)
+
+
+        # print("Positive Sentiment Percentage :- " + str(x))
+        # print("Negative Sentiment Percentage :- " + str(y))
+        # print("Neutral Sentiment Percentage :- " + str(round(100 - (x + y), 1)))
+
+        return render_template('result_user.html', result=fetched_tweets, pos=x, neg=y, neu=z)
 
 @app.route("/predict_sentence",methods=['POST','GET'])
 def predict_text():
